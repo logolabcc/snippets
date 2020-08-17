2755468f43a8949521e2b4330f5283b95f069746
Amar Bešlija <amarbeslija13@gmail.com>
2020-08-17 17:51:01 +0200

    Added Singleton MySQLi Class

--- php.php
+++ php.php
@@ -13,6 +13,7 @@ function cleanString($string){
 /*** E: Function which cleans string  from special characters and spaces ***/
 
 
+
 /*** S: Function which checks does username exists in the database and returns true or false ***/
 
 // PS: Change table name here accordingly
@@ -42,3 +43,53 @@ function checkUsernameExists($data){
     
 }
 /*** E: Function which checks does username exists in the database and returns true or false ***/
+
+
+/********** CLASSES **********/
+
+
+/*** S: Singleton class which connects to the database ***/
+
+class Connection {
+	private $_connection;
+
+	private $_hostname = "localhost";
+	private $_username = "linked_05122020web1_appuser";
+	private $_password = "Database.05122020.Web.APP.LinkedInk!";
+	private $_database = "linked_05122020web1_appdb";
+
+	private static $_instance; 
+
+    // Create instance of the class
+	public static function instance() {
+		
+		if(!self::$_instance) { 
+		    
+		    // If no instance then make one here
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	// Constructor which connects to the database
+	private function __construct() {
+		$this->_connection = new mysqli($this->_host, $this->_username, 
+			$this->_password, $this->_database);
+	
+		// Write that we can't connect to the database if we have an error
+		if(mysqli_connect_error()) {
+			trigger_error("Failed to connect to MySQL: " . mysql_connect_error(),
+				 E_USER_ERROR);
+		}
+	}
+
+	// Magic method clone is empty to prevent duplication of connection
+	private function __clone() { }
+
+	// Get mysqli connection
+	public function connect() {
+		return $this->_connection;
+	}
+}
+
+/*** E: Singleton class which connects to the database ***/
