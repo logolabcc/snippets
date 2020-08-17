c58fe3918f19f99d24d8aee94d4a55846bfe72a1
Amar Bešlija <amarbeslija13@gmail.com>
2020-08-17 17:49:32 +0200

    Added function which checks username in the database

--- php.php
+++ php.php
@@ -13,3 +13,32 @@ function cleanString($string){
 /*** E: Function which cleans string  from special characters and spaces ***/
 
 
+/*** S: Function which checks does username exists in the database and returns true or false ***/
+
+// PS: Change table name here accordingly
+// PPS: Connection class is singleton, so use something similar or change accordingly
+function checkUsernameExists($data){
+    // Get the data and clean it
+    $username = $data['username'];
+    $username = cleanString($username);
+    
+    // Get connection to the database
+    $connection = Connection::instance();
+    $db = $connection->connect();
+    
+    // Check does user with the username provided exists inside the database
+    $sql = "SELECT username FROM Wo_Users WHERE username LIKE '%{$username}%'";
+    
+    // Check do query works
+    if($result = $db->query($sql)){
+        // Check if we have results. If we have, then we are returning true (username already in database)
+        if(mysqli_num_rows($result) >= 1){
+            // Username exists
+            return true;   
+        }
+    }
+    // If we don't have results, username doesn't exists
+    return false;
+    
+}
+/*** E: Function which checks does username exists in the database and returns true or false ***/
